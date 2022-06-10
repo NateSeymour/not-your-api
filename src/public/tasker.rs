2badd043316c6e538e205f2a8e22cc1ec2b85247
Nathan Seymour <nathan@seymour.global>
2022-06-10 19:28:54 -0300

    add routes to add a task and get all tasks

--- src/public/tasker.rs
+++ src/public/tasker.rs
@@ -1,16 +1,70 @@
 use aws_sdk_dynamodb::model::AttributeValue;
+use rocket::serde::json::Json;
+use uuid::Uuid;
+use std::iter::Iterator;
+use tokio_stream::StreamExt;
+use std::boxed::Box;
+use std::error::Error;
 
-#[rocket::post("/create")]
-pub async fn create(db_client: &rocket::State<aws_sdk_dynamodb::Client>) -> Result<(), ()> {
+#[derive(serde::Deserialize)]
+pub struct CreateTaskRB<'r> {
+    description: &'r str,
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Task {
+    id: String,
+    description: String,
+    completed: bool,
+}
+
+#[derive(serde::Serialize)]
+pub struct TaskList {
+    tasks: Vec<Task>,
+}
+
+#[rocket::post("/task", data = "<task>")]
+pub async fn create_task(task: Json<CreateTaskRB<'_>>, db_client: &rocket::State<aws_sdk_dynamodb::Client>) -> Result<(), ()> {
+    let new_task = Task {
+        id: Uuid::new_v4().to_string(),
+        description: task.description.to_string(),
+        completed: false,
+    };
+
+    let item = serde_dynamo::to_item(new_task).unwrap();
+
     db_client.put_item()
         .table_name("NYS_tasker")
-        .item("TaskID", AttributeValue::N(1.to_string()))
-        .item("Penis", AttributeValue::S("Large".into()))
+        .set_item(Some(item))
         .send().await;
 
     Ok(())
 }
 
+#[rocket::get("/task/all")]
+pub async fn get_all_tasks(db_client: &rocket::State<aws_sdk_dynamodb::Client>) -> Result<Json<TaskList>, ()> {
+    let result = db_client.scan()
+        .table_name("NYS_tasker")
+        .send().await.unwrap();
+
+    // And deserialize them as strongly-typed data structures
+    if let Some(items) = result.items {
+        let tasks: Vec<Task> = serde_dynamo::from_items(items).unwrap();
+        println!("Got {} tasks", tasks.len());
+
+        return Ok(Json(TaskList { tasks }));
+    }
+
+    return Err(());
+}
+
+/*
+#[rocket::get("/task/<id>")]
+pub async fn get_task(id: &str, db_client: &rocket::State<aws_sdk_dynamodb::Client>) -> Json<Task<'_>> {
+
+}
+*/
+
 pub fn routes() -> Vec<rocket::Route> {
-    rocket::routes![create]
+    rocket::routes![create_task, get_all_tasks]
 }
